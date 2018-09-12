5199df5ca8d7b841a7b83dfa93fda9afb42d2c91
Cynthia Higley <calgonquin12@yahoo.com>
2018-09-11 20:57:01 -0600

    Tues Submit

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var random="computer";
+
+alert("Please pick a random letter");
+
+function randomWordDashLines{
+    var randomLength = random.length;
+    
+    for (i=0; i< random.length; i++){
+        
+    }
+}
